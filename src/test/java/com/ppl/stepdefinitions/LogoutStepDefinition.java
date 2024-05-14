aa33f790dcac33b5e546af302b9c332914b1bda2
Jovan Shelomo <jovanshelomo@gmail.com>
2024-05-14 09:12:01 +0700

    update

--- src/test/java/com/ppl/stepdefinitions/LogoutStepDefinition.java
+++ src/test/java/com/ppl/stepdefinitions/LogoutStepDefinition.java
@@ -21,28 +21,28 @@ public class LogoutStepDefinition {
         driver = new ChromeDriver();
     }
 
-    @Given("I am logged in")
-    public void I_am_logged_in() {
+    @Given("Pengguna sudah login dengan berhasil")
+    public void Pengguna_sudah_login_dengan_berhasil() {
         driver.get("https://www.saucedemo.com/");
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
         driver.findElement(By.id("login-button")).click();
     }
 
-    @When("I click on the menu button")
-    public void I_click_on_the_menu_button() {
+    @When("Pengguna menekan tombol menu bar")
+    public void Pengguna_menekan_tombol_menu_bar() {
         driver.findElement(By.id("react-burger-menu-btn")).click();
         // wait for the menu to be displayed
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
     }
 
-    @And("I click on the logout button")
-    public void I_click_on_the_logout_button() {
+    @And("Pengguna menekan tombol logout")
+    public void Pengguna_menekan_tombol_logout() {
         driver.findElement(By.id("logout_sidebar_link")).click();
     }
 
-    @Then("I should be logged out")
-    public void I_should_be_logged_out() {
+    @Then("Aplikasi menampilkan halaman login")
+    public void Aplikasi_menampilkan_halaman_login() {
         assertTrue(driver.findElement(By.cssSelector("[data-test='login-container']")).isDisplayed());
     }
 
